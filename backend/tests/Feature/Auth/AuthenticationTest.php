d32b277149e28eb30601e55d2dbdb24bd7f97c2e
Rogerio Pereira <hi@rogeriopereira.dev>
2024-06-14 13:25:00 -0400

    test(backend): Login and Register must create a new api token in database

--- backend/tests/Feature/Auth/AuthenticationTest.php
+++ backend/tests/Feature/Auth/AuthenticationTest.php
@@ -26,7 +26,39 @@ class AuthenticationTest extends TestCase
                     'name' => $user->name,
                     'email' => $user->email,
                 ]
+            ])
+            ->assertJsonStructure([
+                'user' => [
+                    'name',
+                    'email',
+                    'role',
+                    'token',    //Must Contain a token
+                ]
             ]);
+    }
+
+    public function test_users_when_authenticate_generate_a_new_token(): void
+    {
+        $user = User::factory()->create();
+
+        $this->assertDatabaseMissing('personal_access_tokens', [
+            'id' => 1,
+            'tokenable_type' => 'App\Models\User',
+            'tokenable_id' => $user->id,
+        ]);
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+
+        $this->assertDatabaseHas('personal_access_tokens', [
+            'id' => 1,
+            'tokenable_type' => 'App\Models\User',
+            'tokenable_id' => $user->id,
+        ]);
     }
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
--- backend/tests/Feature/Auth/RegistrationTest.php
+++ backend/tests/Feature/Auth/RegistrationTest.php
@@ -21,10 +21,42 @@ class RegistrationTest extends TestCase
         $this->assertAuthenticated();
         $response->assertStatus(201);
         $response->assertJson([
-            'user' => [
-                'name' => 'Test User',
-                'email' => 'test@example.com',
-            ]
+                'user' => [
+                    'name' => 'Test User',
+                    'email' => 'test@example.com',
+                ]
+            ])
+            ->assertJsonStructure([
+                'user' => [
+                    'name',
+                    'email',
+                    'role',
+                    'token',    //Must Contain a token
+                ]
+            ]);
+    }
+
+    public function test_users_when_register_generate_a_new_token(): void
+    {
+        $user = User::factory()->create();
+
+        $this->assertDatabaseMissing('personal_access_tokens', [
+            'id' => 1,
+            'tokenable_type' => 'App\Models\User',
+            'tokenable_id' => $user->id,
+        ]);
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+
+        $this->assertDatabaseHas('personal_access_tokens', [
+            'id' => 1,
+            'tokenable_type' => 'App\Models\User',
+            'tokenable_id' => $user->id,
         ]);
     }
 }
